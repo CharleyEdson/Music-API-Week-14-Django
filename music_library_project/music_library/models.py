6428c7c5bf0835802d6397c2652db78f338dfb88
Charley <charley1135@gmail.com>
2022-11-30 16:02:59 -0800

    feat: Added like field.

--- music_library_project/music_library/models.py
+++ music_library_project/music_library/models.py
@@ -7,3 +7,4 @@ class Music_library(models.Model):
     album = models.CharField(max_length=255)
     release_date = models.DateField()
     genre = models.CharField(max_length=255)
+    likes = models.IntegerField(default=0)
--- music_library_project/music_library/serializers.py
+++ music_library_project/music_library/serializers.py
@@ -4,5 +4,5 @@ from .models import Music_library
 class music_librarySerializer(serializers.ModelSerializer):
     class Meta:
         model = Music_library
-        fields = ['title','artist', 'album','release_date','genre']
+        fields = ['title','artist', 'album','release_date','genre','likes']
         
